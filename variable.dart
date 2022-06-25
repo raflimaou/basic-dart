6c683b7def76a0befd6c23df40e5fb69f015fef4
Rafli Maulana <rafli.maou17@gmail.com>
2022-06-25 17:11:10 +0700

    learn dart variable

---
+++ variable.dart
@@ -0,0 +1,28 @@
+// var name = 'Voyager I';
+// var year = 1977;
+// var antennaDiameter = 3.7;
+// var flybyObjects = ['Jupiter', 'Saturn', 'Uranus', 'Neptune'];
+// var image = {
+//   'tags': ['saturn'],
+//   'url': '//path/to/saturn.jpg'
+// };
+
+void main() {
+  var name = 'Rafli Maulana';
+  var date = 17;
+  var bulan = 'Mei';
+  var tahun = 1999;
+  var angkaKoma = 17.423;
+
+  print('nama saya: $name');
+  print('tanggal lahir saya: ${date}');
+}
+
+// void printInteger(int aNumber) {
+//   print('The number is $aNumber.'); // Print to console.
+// }
+
+// void main() {
+//   var number = 231; // Declare and initialize a variable.
+//   printInteger(number); // Call a function.
+// }
