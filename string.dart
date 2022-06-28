f5df022036ee1ad154dc7770253cad022d008545
Rafli Maulana <rafli.maou17@gmail.com>
2022-06-28 15:48:58 +0700

    learn string

---
+++ string.dart
@@ -0,0 +1,26 @@
+// ignore_for_file: unused_local_variable
+
+import 'dart:ffi';
+
+void main() {
+  // var angka = 17;
+  // String name = angka.toString();
+
+  String name = 'Rafli Maulana';
+
+  var number = 17;
+
+  String daftarHewan = 'Ayam, Bebek, Kucing';
+
+  print('angka $number');
+
+  print(name.toLowerCase());
+
+  print(name.contains('Raf'));
+
+  print(name.toUpperCase());
+
+  print(daftarHewan.split(',')[0]);
+
+  print(name.substring(0, 4));
+}
